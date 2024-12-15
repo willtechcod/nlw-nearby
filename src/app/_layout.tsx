b94c732f2617512b4930cfaa369cf8e59beaee79
willtechcod <dev.pereira2019@gmail.com>
2024-12-15 20:25:15 -0300

    feat create components Places and Place, configure style components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import {
     useFonts,
@@ -24,11 +25,13 @@ export default function Layout(){
     }
 
     return (
+        <GestureHandlerRootView style={{ flex: 1}}>
         <Stack  
         screenOptions={{ 
             headerShown: false,
             contentStyle: { backgroundColor: colors.gray[100]},
         }} 
         />
+        </GestureHandlerRootView>
     )
 }
---
+++ src/app/home.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+
+import { api } from "@/services/api";
+
+import { Places } from "@/components/places";
+import { PlaceProps } from "@/components/place";
+import { Categories, CategoriesProps } from "@/components/categories";
+
+type MarketsProps = PlaceProps;
+
+export default function Home(){
+const [categories, setCategories] = useState<CategoriesProps>([]);
+const [category, setCategory] = useState("");
+const [markets, setMarkets] = useState<MarketsProps[]>([]);
+
+    async function fetchCategories() {
+        try {
+            const { data } = await api.get("/categories");
+            setCategories(data);
+            setCategory(data[0].id);
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Ops", "Não foi possivel carregar as categorias!")
+        }
+    }
+
+    async function fetchMarkets() {
+        try {
+            if (!category){
+                return
+            }
+
+            const { data } = await api.get("/markets/category/" + category);
+            setMarkets(data);
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Ops', "Não foi possível carregar os locais.")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    useEffect(() => {
+        fetchMarkets();
+    }, [category]);
+
+    return (
+        <View style={{ flex: 1, backgroundColor: "#cecece" }}>
+          <Categories 
+          data={categories} 
+          onSelect={setCategory} 
+          selected={category}
+          />
+
+          <Places data={markets} />
+        </View>
+    )
+}
